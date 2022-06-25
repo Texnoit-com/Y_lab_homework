eb4181f8acfbb888239b7aedbf4d2c06630b567f
Maxim Bekurin <BekurinMD@yandex.ru>
2022-06-25 11:32:04 +0500

    homework-2 version 1.0

--- homework_2/example1.py
+++ homework_2/example1.py
@@ -1,5 +1,5 @@
-'''Надо написать класс CyclicIterator. Итератор должен итерироваться по 
-итерируемому объекту (list, tuple, set, range, Range2, и т. д.), и когда 
+'''Надо написать класс CyclicIterator. Итератор должен итерироваться по
+итерируемому объекту (list, tuple, set, range, Range2, и т. д.), и когда
 достигнет последнего элемента, начинать сначала.'''
 
 from dataclasses import dataclass
@@ -7,14 +7,23 @@ from dataclasses import dataclass
 
 @dataclass
 class CyclicIterator:
+    num: list
+    index: int = 0
+
     def __iter__(self):
-        pass
+        return self
 
     def __next__(self):
-        pass
+        if self.index < len(self.num):
+            result = self.num[self.index]
+            self.index += 1
+        else:
+            self.index = 0
+            result = self.num[self.index]
+        return result
 
 
 if __name__ == '__main__':
-    cyclic_iterator = CyclicIterator(range(3))
+    cyclic_iterator = CyclicIterator(range(100))
     for i in cyclic_iterator:
         print(i)
--- homework_2/example2.py
+++ homework_2/example2.py
@@ -1,6 +1,6 @@
 '''У каждого фильма есть расписание, по каким дням он идёт в кинотеатрах. Для
 эффективности дни проката хранятся периодами дат. Например, прокат фильма
-проходит с 1 по 7 января, а потом показ возобновляется 
+проходит с 1 по 7 января, а потом показ возобновляется
 с 15 января по 7 февраля:'''
 
 
@@ -15,7 +15,12 @@ class Movie:
     dates: List[Tuple[datetime, datetime]]
 
     def schedule(self) -> Generator[datetime, None, None]:
-        return []
+        step_range: timedelta = timedelta(days=1)
+        for season in self.dates:
+            show_start: datetime = season[0]
+            while show_start <= season[1]:
+                yield str(show_start)
+                show_start += step_range
 
 
 if __name__ == '__main__':
@@ -23,6 +28,5 @@ if __name__ == '__main__':
         (datetime(2020, 1, 1), datetime(2020, 1, 7)),
         (datetime(2020, 1, 15), datetime(2020, 2, 7))
     ])
-
     for d in m.schedule():
         print(d)
